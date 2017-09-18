b1bd748a36ade4815f6254021367927f35c0d141
xqn <xqn@xqn.me>
2017-09-18 22:01:36 +0800

    x

---
+++ resources/views/Home/sign/test.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8">
+            <table class="table table-hover">
+                <tbody>
+                    <tr>
+                        <td>手机号</td>
+                        <td>密码</td>
+                    </tr>
+                    <tr class="active">
+                        <td><input type="" name=""></td>
+                        <td><input type="" name=""></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="jquery/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
