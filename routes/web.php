b6fc70192307b7912db871c360a65a4d2339822a
wh <1416724891@qq.com>
2017-09-18 14:13:50 +0800

    zmhb

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//
+//});
+
+//前台
+//Route::get('/home/content' , function(){
+//    return view('home/content');
+//});
+
+//文章内容
+Route::get('home/content/{id}' , 'Home\ContentController@content');
+
+//文章收藏
+Route::get('home/conllect' , 'Home\ConllectController@conllect');
+Route::get('home/conllect/insert/{id}' , 'Home\ConllectController@insert');
+Route::get('home/conllect/delete/{id}' , 'Home\ConllectController@delete');
+//Route::get('home/userarticle/{id}' , 'Home\UserarticleController@userarticle');
+
+
+//关注
+Route::get('home/attention' , 'Home\AttentionController@attention');
+Route::get('home/attention/insert/{id}' , 'Home\AttentionController@insert');
+Route::get('home/attention/delete/{id}' , 'Home\AttentionController@delete');
+Route::get('home/userarticle/{id}' , 'Home\UserarticleController@userarticle');
+
+//站内信息管理
+Route::get('home/messages' , 'Home\MessagesController@messages');
+//Route::get('home/messages', 'Home\MessagesController@delete');
