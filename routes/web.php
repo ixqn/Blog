817806493082b75a1f16ffba00fc9984d50142d0
lovesevenworld <964518182@qq.com>
2017-09-18 16:22:52 +0800

    cc

--- routes/web.php
+++ routes/web.php
@@ -63,30 +63,6 @@ Route::post('/is_emailActive', 'home\verifyController@is_emailActive');
 
 
 
-// zhangyu
-//文章列表
-Route::get('admin/article','Admin\ArticleController@index');
-//文章内容单页
-Route::get('admin/article/cont/{id}','Admin\ArticleController@cont');
-//显示文章
-Route::post('admin/article/show/{id}','Admin\ArticleController@show');
-//删除文章
-
-//分类管理模块
-Route::resource('admin/category','Admin\CategoryController');
-//分类排序字段
-Route::post('admin/category/changeorder','Admin\CategoryController@changeOrder');
-
-//举报文章
-Route::get('admin/inf/article','Admin\InfController@index');
-//举报文章处理
-Route::post('admin/inf/dis/{id}','Admin\InfController@dis');
-
-//举报评论
-Route::get('admin/inf/comment','Admin\InfController@show');
-Route::post('admin/inf/discom/{id}','Admin\InfController@discom');
-
-
 
 
 
@@ -147,6 +123,32 @@ Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>'Login'],func
     //资源
     Route::resource('admin','AdminController');
     Route::resource('users','UsersController');
+
+
+
+// zhangyu
+//文章列表
+    Route::get('article','ArticleController@index');
+//文章内容单页
+    Route::get('article/cont/{id}','ArticleController@cont');
+//显示文章
+    Route::post('article/show/{id}','ArticleController@show');
+//删除文章
+
+//分类管理模块
+    Route::resource('category','CategoryController');
+//分类排序字段
+    Route::post('category/changeorder','CategoryController@changeOrder');
+
+//举报文章
+    Route::get('inf/article','InfController@index');
+//举报文章处理
+    Route::post('inf/dis/{id}','InfController@dis');
+
+//举报评论
+    Route::get('inf/comment','InfController@show');
+    Route::post('inf/discom/{id}','InfController@discom');
+
 
 });
 
